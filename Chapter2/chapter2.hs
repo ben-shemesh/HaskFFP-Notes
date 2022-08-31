65d17ccd6c25e6e62b1d0c1a9c3a007ad1029453
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-08-30 22:21:27 -0400

    Added notes on abstracting away with functions and there lambda origin of functions in Haskells.

--- Chapter2/chapter2.hs
+++ Chapter2/chapter2.hs
@@ -3,4 +3,8 @@ sayHello x =
     putStrLn ("Hello, " ++ x ++ "!")
 -- the (::) symbols represents the way to write a type signature.
     -- (foo has) (has the  type {::} ) (of pooh)
-    
+triple :: Num a => a -> a -> a
+triple x y = (x + y) * 3
+-- triple takes two arguments of type Num and returns a Num
+-- abstracts away from: (3 + 5) * 3
+                -- or   (2 + 6) * 3
